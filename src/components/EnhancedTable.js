adbcf254572ee0688a96642a591ef8ded73df6cc
Sukhchand <sukhchand38@gmail.com>
2021-07-05 20:06:17 +0530

    initial changes

---
+++ src/components/EnhancedTable.js
@@ -0,0 +1,370 @@
+import React from "react";
+
+import Checkbox from "@material-ui/core/Checkbox";
+import MaUTable from "@material-ui/core/Table";
+import PropTypes from "prop-types";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TablePaginationActions from "./TablePaginationActions";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import TableToolbar from "./TableToolbar";
+import update from "immutability-helper";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import {HTML5Backend} from 'react-dnd-html5-backend';
+import menuIcon from '../assets/menu.png';
+import {
+  useGlobalFilter,
+  usePagination,
+  useRowSelect,
+  useSortBy,
+  useTable,
+} from "react-table";
+
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef();
+    const resolvedRef = ref || defaultRef;
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
+
+    return (
+      <>
+        <Checkbox ref={resolvedRef} {...rest} />
+      </>
+    );
+  }
+);
+
+const inputStyle = {
+  padding: 0,
+  margin: 0,
+  border: 0,
+  background: "transparent",
+};
+
+const DND_ITEM_TYPE = "row";
+const EditableCell = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onBlur = () => {
+    updateMyData(index, id, value);
+  };
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return (
+    <input
+      style={inputStyle}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+    />
+  );
+};
+
+EditableCell.propTypes = {
+  cell: PropTypes.shape({
+    value: PropTypes.any.isRequired,
+  }),
+  row: PropTypes.shape({
+    index: PropTypes.number.isRequired,
+  }),
+  column: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }),
+  updateMyData: PropTypes.func.isRequired,
+};
+
+// Set our editable cell renderer as the default Cell renderer
+const defaultColumn = {
+  Cell: EditableCell,
+};
+
+const EnhancedTable = ({
+  columns,
+  data,
+  setData,
+  updateMyData,
+  skipPageReset,
+}) => {
+  const {
+    getTableProps,
+    headerGroups,
+    prepareRow,
+    page,
+    gotoPage,
+    setPageSize,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    state: { pageIndex, pageSize, selectedRowIds, globalFilter },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      autoResetPage: !skipPageReset,
+      updateMyData,
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination,
+    useRowSelect,
+    (hooks) => {
+      hooks.allColumns.push((columns, index) => [
+        {
+          id: "move",
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <>
+              <div>
+                
+              </div>
+            </>
+          ),
+          Cell: ({ row }) => (
+            <div style={{display:'none'}}></div>
+          ),
+        },
+        {
+          id: "selection",
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <>
+              <div>
+                <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+              </div>
+            </>
+          ),
+          Cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          ),
+        },
+        ...columns,
+      ]);
+    }
+  );
+
+  const handleChangePage = (event, newPage) => {
+    gotoPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPageSize(Number(event.target.value));
+  };
+
+  const removeByIndexs = (array, indexs) =>
+    array.filter((_, i) => !indexs.includes(i));
+
+  const deleteUserHandler = (event) => {
+    const newData = removeByIndexs(
+      data,
+      Object.keys(selectedRowIds).map((x) => parseInt(x, 10))
+    );
+    setData(newData);
+  };
+
+  const addByIndexs = (array, indexs) =>
+    array.filter((_, i) => indexs.includes(i));
+
+  const copyUserHandler = (event) => {
+    let copyData = [...data];
+    const newData = addByIndexs(
+      data,
+      Object.keys(selectedRowIds).map((x) => parseInt(x, 10))
+    )[0];
+    copyData = data.concat(newData);
+    setData(copyData);
+  }
+
+  const addUserHandler = (user) => {
+    const newData = data.concat([user]);
+    setData(newData);
+  };
+
+  const moveRow = (dragIndex, hoverIndex) => {
+    const dragRecord = data[dragIndex];
+    setData(
+      update(data, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragRecord],
+        ],
+      })
+    );
+  };
+
+  // Render the UI for your table
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <TableContainer>
+        <TableToolbar
+          numSelected={Object.keys(selectedRowIds).length}
+          deleteUserHandler={deleteUserHandler}
+          addUserHandler={addUserHandler}
+          preGlobalFilteredRows={preGlobalFilteredRows}
+          setGlobalFilter={setGlobalFilter}
+          globalFilter={globalFilter}
+          copyUserHandler={copyUserHandler}
+        />
+        <MaUTable {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map((headerGroup) => (
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <TableCell
+                    {...(column.id === "selection"
+                      ? column.getHeaderProps()
+                      : column.getHeaderProps(column.getSortByToggleProps()))}
+                  >
+                    {column.render("Header")}
+                    {column.id !== "selection" ? (
+                      <TableSortLabel
+                        active={column.isSorted}
+                        // react-table has a unsorted state which is not treated here
+                        direction={column.isSortedDesc ? "desc" : "asc"}
+                      />
+                    ) : null}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {page.map((row, i) => {
+              prepareRow(row);
+              return (
+                <Row
+                  index={i}
+                  row={row}
+                  moveRow={moveRow}
+                  {...row.getRowProps()}
+                >
+                  <TableRow {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <TableCell {...cell.getCellProps()}>
+                          {cell.render("Cell")}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                </Row>
+              );
+            })}
+          </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[
+                  5,
+                  10,
+                  25,
+                  { label: "All", value: data.length },
+                ]}
+                colSpan={3}
+                count={data.length}
+                rowsPerPage={pageSize}
+                page={pageIndex}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </MaUTable>
+      </TableContainer>
+    </DndProvider>
+  );
+};
+
+const Row = ({ row, index, moveRow }) => {
+  const dropRef = React.useRef(null);
+  const dragRef = React.useRef(null);
+
+  const [, drop] = useDrop({
+    accept: DND_ITEM_TYPE,
+    hover(item, monitor) {
+      if (!dropRef.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const hoverBoundingRect = dropRef.current.getBoundingClientRect();
+
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      const clientOffset = monitor.getClientOffset();
+
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      moveRow(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag, preview] = useDrag({
+    type: "row",
+    item: { type: "row", index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const opacity = isDragging ? 0 : 1;
+
+  preview(drop(dropRef));
+  drag(dragRef);
+
+  return (
+    <tr ref={dropRef} style={{ opacity }}>
+      <td ref={dragRef} className="drag-icon"><img src={menuIcon}/></td>
+      {row.cells.map((cell) => {
+        return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+      })}
+    </tr>
+  );
+};
+
+EnhancedTable.propTypes = {
+  columns: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
+  updateMyData: PropTypes.func.isRequired,
+  setData: PropTypes.func.isRequired,
+  skipPageReset: PropTypes.bool.isRequired,
+};
+
+export default EnhancedTable;
